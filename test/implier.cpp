90af0165e573834580dd858dd49f0025bbbc87f0
Ned Bingham <eab242@cornell.edu>
2016-07-28 23:16:55 -0700

    tested implier

---
+++ test/implier.cpp
@@ -0,0 +1,62 @@
+#include <gtest/gtest.h>
+
+#include <core/implier.h>
+
+using namespace core;
+
+TEST(implier, compare)
+{
+	EXPECT_FALSE((implier<int, int>(5, 10) < implier<int, int>(2, 16)));
+	EXPECT_FALSE((implier<int, int>(5, 16) < implier<int, int>(2, 10)));
+	EXPECT_TRUE ((implier<int, int>(2, 10) < implier<int, int>(5, 16)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) < implier<int, int>(5, 10)));
+	EXPECT_FALSE((implier<int, int>(5, 16) < implier<int, int>(2, 16)));
+	EXPECT_FALSE((implier<int, int>(2, 16) < implier<int, int>(2, 10)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) < implier<int, int>(5, 16)));
+	EXPECT_FALSE((implier<int, int>(2, 16) < implier<int, int>(2, 10)));
+
+	EXPECT_TRUE ((implier<int, int>(5, 10) > implier<int, int>(2, 16)));
+	EXPECT_TRUE ((implier<int, int>(5, 16) > implier<int, int>(2, 10)));
+	EXPECT_FALSE((implier<int, int>(2, 10) > implier<int, int>(5, 16)));
+	EXPECT_FALSE((implier<int, int>(2, 16) > implier<int, int>(5, 10)));
+	EXPECT_TRUE ((implier<int, int>(5, 16) > implier<int, int>(2, 16)));
+	EXPECT_FALSE((implier<int, int>(2, 16) > implier<int, int>(2, 10)));
+	EXPECT_FALSE((implier<int, int>(2, 16) > implier<int, int>(5, 16)));
+	EXPECT_FALSE((implier<int, int>(2, 16) > implier<int, int>(2, 10)));
+
+	EXPECT_FALSE((implier<int, int>(5, 10) <= implier<int, int>(2, 16)));
+	EXPECT_FALSE((implier<int, int>(5, 16) <= implier<int, int>(2, 10)));
+	EXPECT_TRUE ((implier<int, int>(2, 10) <= implier<int, int>(5, 16)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) <= implier<int, int>(5, 10)));
+	EXPECT_FALSE((implier<int, int>(5, 16) <= implier<int, int>(2, 16)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) <= implier<int, int>(2, 10)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) <= implier<int, int>(5, 16)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) <= implier<int, int>(2, 10)));
+
+	EXPECT_TRUE ((implier<int, int>(5, 10) >= implier<int, int>(2, 16)));
+	EXPECT_TRUE ((implier<int, int>(5, 16) >= implier<int, int>(2, 10)));
+	EXPECT_FALSE((implier<int, int>(2, 10) >= implier<int, int>(5, 16)));
+	EXPECT_FALSE((implier<int, int>(2, 16) >= implier<int, int>(5, 10)));
+	EXPECT_TRUE ((implier<int, int>(5, 16) >= implier<int, int>(2, 16)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) >= implier<int, int>(2, 10)));
+	EXPECT_FALSE((implier<int, int>(2, 16) >= implier<int, int>(5, 16)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) >= implier<int, int>(2, 10)));
+
+	EXPECT_FALSE((implier<int, int>(5, 10) == implier<int, int>(2, 16)));
+	EXPECT_FALSE((implier<int, int>(5, 16) == implier<int, int>(2, 10)));
+	EXPECT_FALSE((implier<int, int>(2, 10) == implier<int, int>(5, 16)));
+	EXPECT_FALSE((implier<int, int>(2, 16) == implier<int, int>(5, 10)));
+	EXPECT_FALSE((implier<int, int>(5, 16) == implier<int, int>(2, 16)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) == implier<int, int>(2, 10)));
+	EXPECT_FALSE((implier<int, int>(2, 16) == implier<int, int>(5, 16)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) == implier<int, int>(2, 10)));
+
+	EXPECT_TRUE ((implier<int, int>(5, 10) != implier<int, int>(2, 16)));
+	EXPECT_TRUE ((implier<int, int>(5, 16) != implier<int, int>(2, 10)));
+	EXPECT_TRUE ((implier<int, int>(2, 10) != implier<int, int>(5, 16)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) != implier<int, int>(5, 10)));
+	EXPECT_TRUE ((implier<int, int>(5, 16) != implier<int, int>(2, 16)));
+	EXPECT_FALSE((implier<int, int>(2, 16) != implier<int, int>(2, 10)));
+	EXPECT_TRUE ((implier<int, int>(2, 16) != implier<int, int>(5, 16)));
+	EXPECT_FALSE((implier<int, int>(2, 16) != implier<int, int>(2, 10)));
+}
